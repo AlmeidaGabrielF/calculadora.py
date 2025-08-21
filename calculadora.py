ce6ba3704d351d034b7cc57b5ccb2d61b6c266e8
AlmeidaGabrielF <gabrielfloriano2005@gmail.com>
2025-08-20 21:22:01 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+N1 = int(input("digite o numero 1:"))
+N2 = int(input("digite o numero 2:"))
+
+adicao = N1 + N2
+subtracao = N1 - N2 
+divisao = N1 / N2
+multiplicacao = N1 * N2
+
+print("resultado da soma:", adicao)
+print("resultado da subtração", subtracao)
+print("resultado da divisao", divisao)
+print("resultado da multiplicação", multiplicacao)
